84804cd0e54d5857f9d80445b05359eb77f48c8e
HarrisAffandi708 <harris.ilhan.bin@gmail.com>
2022-07-31 13:26:25 +0100

    added UI file

--- covid_news_handling.py
+++ covid_news_handling.py
@@ -1,24 +1,30 @@
 import json
 import datetime
 import requests
+import sched, time
 
+#open config file to read the necessary data
 with open("config/config.json", "r") as config_file:
     news_config = json.load(config_file)
     API_key = news_config["News API Configuration"]["APIKey"]
-
-articles = []
-
 
 def news_API_request(covid_terms:str="Covid COVID-19 coronavirus"):
     covid_terms = "Covid OR COVID-19 OR coronavirus"
     current_date = datetime.date.today()
     url = ('https://newsapi.org/v2/everything?q='f'{covid_terms}&from={current_date}&language=en&sortBy=popularity&apiKey={API_key}')
     news_api_request = requests.get(url).json()
+    news_articles = open("data/articles.json", "w")
+    news_articles.write(json.dumps(news_api_request, sort_keys=False, indent=2))
+    return news_API_request
     
-    print(news_api_request)
+    
 
-def update_news(news_API_request):
-    x= 1
+def update_news():
+    news_API_request()
+    return news_API_request
 
-
-news_API_request()
+def schedule_news_updates(update_interval:int):
+    updates = sched.scheduler(time.time, time.sleep)
+    updates.enter(float(update_interval),1,update_news())
+    updates.run()
+    return update_interval
