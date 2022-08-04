9e6e1c4223c834ca69eaf4e1f7cd483b0861b47d
HarrisAffandi708 <harris.ilhan.bin@gmail.com>
2022-08-04 15:17:47 +0100

    updated

--- user_interface.py
+++ user_interface.py
@@ -12,10 +12,12 @@ logging.basicConfig(level=logging.DEBUG,filename="logs.log", encoding='utf-8',fo
 
 data_sched = sched.scheduler(time.time, time.sleep)
 news_sched = sched.scheduler(time.time, time.sleep)
+data_news_sched = sched.scheduler(time.time, time.sleep)
 
 data_update_info = []
 news_update_info = []
 overall_updates_dict = {}
+double_update_info = []
 
 def time_difference(update):
     hours_mins = update.split(':')
@@ -70,21 +72,34 @@ def sched_data_update(update_name, update_time, update_repeat):
         data_update_info.append({"update_name":update_name,"update_time":update_time,"update_repeat": update_repeat})
         sched_data_update = data_sched.enterabs(data_update_time,1,update_data)
         overall_updates_dict[update_name] = sched_data_update
-        logging.debug("succefully scheduled an update")
+        logging.info("succefully scheduled an update")
     else:
         logging.error("scheduled update is already existing")
+    return data_update_info
 
-def schedule_news_updates(update_name, update_time,update_repeat):
+def sched_news_update(update_name, update_time,update_repeat):
     news_update_time = time_difference(update_time)
     if update_time not in news_update_info:
-        news_update_info.append(update_time)
+        news_update_info.append({"update_time":update_time,"update_repeat":update_repeat})
         sched_news_update = news_sched.enterabs(news_update_time,1,update_news)
         overall_updates_dict[update_name] = sched_news_update
-        logging.debug("succefully scheduled news update")
+        logging.info("succefully scheduled news update")
     else:
         logging.error("scheduled update is alrready existing")
+    return news_update_info
 
-@app.route('/', methods=['GET','post'])
+def sched_data_news_update(update_name,update_time,update_repeat):
+    data_news_time = time_difference(update_time)
+    if update_time in news_update_info or data_update_info or double_update_info:
+        logging.error("already scheduled an update at this time")
+    else:
+        double_update_info.append(update_time)
+        sched_double_update = data_news_sched.enterabs(data_news_time,1,update_data_news)
+        overall_updates_dict[update_name] =sched_double_update
+    return double_update_info
+
+
+@app.route('/', methods=['GET','POST'])
 def index():
     #retreives the keywords from the url when an action from the user is made, e.g updating name
     cancel_update = request.args.get('update_item')
@@ -96,7 +111,26 @@ def index():
     cancel_news = request.args.get('notif')
 
     #scheduling section done according to user actions, since update_name is a required field. we will use it as the root if statement
-    #if update_name:
+    if update_name:
+        #checks whether the user checked the update news and covid data boxes
+        if update_data =="covid-data" and update_news=="news":
+            sched_data_news_update(update_name,update_time,repeat_update)
+            logging.info("news and data update")
+        elif update_data =="covid-data":
+            sched_data_update(update_name,update_time,repeat_update)
+        elif update_news =="news":
+            sched_news_update(update_name,update_time,repeat_update)
+    
+    if removed_article != None:
+        removed_article_title = str(removed_article).replace("+","")
+        if removed_article_title not in removed_article:
+            removed_article.append(removed_article_title)
+        for i in news:
+            if i["title"] ==removed_article:
+                news.remove(i)
+                logging.info("news article removed")
+                add_news()
+                logging.info("news article added")
 
 
     return render_template("index.html", title= "Covid Data",location=exeter_name, local_7day_infections= exeter_7_day_cases, nation_location=nation_name,
