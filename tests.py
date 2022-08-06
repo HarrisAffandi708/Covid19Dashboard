fd85d47e9d3e6df2170dc88162eb3170a02a5daa
HarrisAffandi708 <harris.ilhan.bin@gmail.com>
2022-08-06 09:21:43 +0100

    update

--- tests.py
+++ tests.py
@@ -1,7 +1,22 @@
-from covid_data_handler import parse_csv_data
+from covid_data_handler import parse_csv_data, process_covid_csv_data
+from covid_news_handling import articles, news_API_request
 
 def test_parse_csv_data():
     data= parse_csv_data('nation_2021-10-28.csv')
     assert len(data) == 639
 
+def test_process_csv_data():
+    last7days_cases , current_hospital_cases , total_deaths = process_covid_csv_data(parse_csv_data('nation_2021-10-28.csv'))
+    assert last7days_cases == 240299
+    assert current_hospital_cases == 7019
+    assert total_deaths == 141544
+
+def test_articles():
+    news_api = news_API_request()
+    news = articles(news_api)
+    assert news
+    assert len(news) == 10
+
+test_parse_csv_data()
+test_articles()
 test_parse_csv_data()
